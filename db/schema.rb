22c3e8bc58834ab9223b6908fe1adbf78a127112
sefty <seftyyunitasari@gmail.com>
2021-12-27 08:27:03 +0900

    create Post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_26_232231) do
+ActiveRecord::Schema.define(version: 2021_12_26_232530) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.text "content"
+    t.text "caption"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -24,4 +33,5 @@ ActiveRecord::Schema.define(version: 2021_12_26_232231) do
     t.index ["email"], name: "index_users_on_email"
   end
 
+  add_foreign_key "posts", "users"
 end
