0496d79c73b5c6c2f47805005bce9e240c610f66
sefty <seftyyunitasari@gmail.com>
2021-12-31 05:50:41 +0900

    create Like model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
   validates :content, presence: true
+  has_many :likes, dependent: :destroy
+  has_many :like_users, through: :likes, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
 	before_validation {email.downcase!}
 	has_secure_password
 	validates :password, length: {minimum: 6}
+	has_many :likes, dependent: :destroy
 end
