37c8d91ec451b4580a683165789e0db7381e8495
sefty <seftyyunitasari@gmail.com>
2022-01-05 14:41:55 +0900

    edit the validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
-  validates :content, presence: true
   has_many :likes, dependent: :destroy
   has_many :like_users, through: :likes, source: :user
   mount_uploader :content, ImageUploader
